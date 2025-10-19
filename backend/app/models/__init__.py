7d149ff926aae26d2a8f0b743366076e80f2f918
Roman Moroz <morozdef24@gmail.com>
2025-10-19 18:29:53 +0300

    init.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+from backend.app.models import (
+    ml_model,  # F401 # noqa: F401
+    user,  # F401 # noqa: F401
+    crypto_data,  # F401 # noqa: F401
+    portfolio,  # F401 # noqa: F401
+    simulation,  # F401 # noqa: F401
+)
