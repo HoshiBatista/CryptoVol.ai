1e9039de01c81473d09d6b002f9eeb223bfe4345
Roman Moroz <morozdef24@gmail.com>
2025-10-19 18:16:58 +0300

    update crypto models

--- backend/app/models/crypto_data.py
+++ backend/app/models/crypto_data.py
@@ -1,15 +1,47 @@
-import uuid
-
 from app.db.base import Base
+from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, String, TIMESTAMP, DECIMAL, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    DECIMAL,
+    Float,
+    ForeignKey,
+    Text,
+)
+
+
+class Cryptocurrency(Base):
+    __tablename__ = "cryptocurrencies"
+
+    id = Column(Integer, primary_key=True)
+    symbol = Column(String(16), unique=True, nullable=False)
+    name = Column(String(100), nullable=False)
+    description = Column(Text)
+
+    data_points = relationship(
+        "CryptocurrencyData", back_populates="crypto", cascade="all, delete-orphan"
+    )
+    models = relationship(
+        "TrainedModel", back_populates="crypto", cascade="all, delete-orphan"
+    )
+    portfolio_assets = relationship(
+        "PortfolioAsset", back_populates="crypto", cascade="all, delete-orphan"
+    )
 
 
 class CryptocurrencyData(Base):
     __tablename__ = "cryptocurrency_data"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    symbol = Column(String, index=True, nullable=False)
-    timestamp = Column(TIMESTAMP, unique=True, nullable=False)
-    price_usd = Column(DECIMAL, nullable=False)
-    daily_return = Column(Float, nullable=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
+    crypto_id = Column(Integer, ForeignKey("cryptocurrencies.id"), nullable=False)
+    timestamp = Column(DateTime(timezone=True), nullable=False)
+    price_usd = Column(DECIMAL(18, 8), nullable=False)
+    daily_return = Column(Float)
+
+    crypto = relationship("Cryptocurrency", back_populates="data_points")
+
+    __table_args__ = ({"sqlite_autoincrement": True},)
